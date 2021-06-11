d6fd8cdd75e803ba3db05854555c4917a410cf94
ybadmus <yusif.badmus@gmail.com>
2021-06-11 19:24:03 +0000

    fix bug with create method saving mulitpe records for recruiter positions

--- app/controllers/recruiters_controller.rb
+++ app/controllers/recruiters_controller.rb
@@ -13,61 +13,43 @@ class RecruitersController < ApplicationController
   # GET /recruiters/new
   def new
     @recruiter = Recruiter.new
-    @defaults = []
+    set_default
   end
 
   # GET /recruiters/1/edit
   def edit
-    @defaults = RecruiterPosition.where(recruiter_id: @recruiter.id).pluck(:position_id)
+    set_default
   end
 
   # POST /recruiters or /recruiters.json
   def create
     @recruiter = Recruiter.new(recruiter_params)
-    params[:recruiter][:position_id].each do |position|
-      if !position.empty?
-        @recruiter.recruiter_positions.build(:position_id => position)
-      end
-    end
 
-    respond_to do |format|
-      if @recruiter.save
-        format.html { redirect_to recruiters_path, notice: "Recruiter was successfully created." }
-        format.json { render :show, status: :created, location: @recruiter }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recruiter.errors, status: :unprocessable_entity }
-      end
+    build_positions @recruiter
+
+    if @recruiter.save
+      redirect_to recruiters_path, notice: "Recruiter was successfully created." 
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /recruiters/1 or /recruiters/1.json
   def update
-    params[:recruiter][:position_id].each do |position|
-      if !position.empty?
-        @recruiter.recruiter_positions.build(:position_id => position)
-      end
-    end
+    build_positions @recruiter
 
-    RecruiterPosition.where(recruiter_id: @recruiter.id).delete_all
-    respond_to do |format|
-      if @recruiter.update(recruiter_params)
-        format.html { redirect_to recruiters_path, notice: "Recruiter was successfully updated." }
-        format.json { render :show, status: :ok, location: @recruiter }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @recruiter.errors, status: :unprocessable_entity }
-      end
+    if update_recruiter_position @recruiter
+      redirect_to recruiters_path, notice: "Recruiter was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /recruiters/1 or /recruiters/1.json
+ # DELETE /recruiters/1 or /recruiters/1.json
   def destroy
     @recruiter.destroy
-    respond_to do |format|
-      format.html { redirect_to recruiters_url, notice: "Recruiter was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "Recruiter was successfully destroyed." 
+    redirect_to recruiters_url
   end
 
   private
@@ -76,8 +58,25 @@ class RecruitersController < ApplicationController
       @recruiter = Recruiter.find(params[:id])
     end
 
+    def build_positions recruiter
+      params[:recruiter][:position_ids].each do |position|
+        if !position.empty?
+          recruiter.recruiter_positions.build(:position_id => position)
+        end
+      end
+    end
+
+    def update_recruiter_position recruiter
+      RecruiterPosition.where(recruiter_id: recruiter.id).delete_all
+      recruiter.update(recruiter_params)
+    end
+
+    def set_default
+      @defaults = !@recruiter.id.nil? ? RecruiterPosition.where(recruiter_id: @recruiter.id).pluck(:position_id) : []
+    end
+
     # Only allow a list of trusted parameters through.
     def recruiter_params
-      params.require(:recruiter).permit(:name, :email, :position_id)
+      params.require(:recruiter).permit(:name, :email, :position_ids)
     end
 end
