497fef931c44ba4c8f0af86ee08c3465d57dcbeb
ybadmus <yusif.badmus@gmail.com>
2021-06-12 12:16:21 +0000

    update candidate create interview form logic in helper file

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -2,4 +2,13 @@ module CandidatesHelper
   def candidate_skills candidate
     (candidate.position.skills.map {|skill| "#{skill.name}"}).join(', ') 
   end
+
+  def schedule_interview_card candidate
+    if candidate.matched
+      raw("<strong>This candidate has already been matched</strong>")
+    else 
+      raw("<strong>Suggested recruiters with the matching skills:</strong>
+      #{render 'interviews/form', score: @score}")
+    end
+  end
 end
