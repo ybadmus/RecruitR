be77cf1a518f21daee2daa8cad5a58862b268acc
ybadmus <yusif.badmus@gmail.com>
2021-06-14 02:46:38 +0000

    add timezone information to emails and invitation

--- app/mailers/interview_mailer.rb
+++ app/mailers/interview_mailer.rb
@@ -2,7 +2,7 @@ class InterviewMailer < ApplicationMailer
   def new_interview_email
     @interview = params[:interview]
 
-    mail(to: "#{@interview.candidate.email},#{@interview.recruiter.email}", subject: "Invitation:  #{@interview.candidate.fname} #{@interview.candidate.lname} and #{@interview.recruiter.name} @ #{Date.parse(@interview.interview_date.split(' ')[0])}, #{@interview.interview_date.split(' ')[1]}")
+    mail(to: "#{@interview.candidate.email},#{@interview.recruiter.email}", subject: "Invitation:  #{@interview.candidate.fname} #{@interview.candidate.lname} and #{@interview.recruiter.name} @ #{Date.parse(@interview.interview_date.split(' ')[0])}, #{@interview.interview_date.split(' ')[1]} #{Time.zone.name}")
   end
 end
 
