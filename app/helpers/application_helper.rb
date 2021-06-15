04924881d7dc695a71d9b3b54109fc324096f756
ybadmus <yusif.badmus@gmail.com>
2021-06-15 18:11:26 +0000

    add styling to footer and navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,15 @@ module ApplicationHelper
   def navbar
     if logged_in?
       raw("<nav class='navbar navbar-expand-lg navbar-light bg-light'><div class='container-fluid'>
-        <a class='navbar-brand' href='#'>RecruitR</a>
+        <a class='navbar-brand' href='/'>RecruitR</a>
         <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'><span class='navbar-toggler-icon'></span></button>
         <div class='collapse navbar-collapse justify-content-end' id='navbarNav'>
           <ul class='navbar-nav'>
             <li class='nav-item'>
-              #{link_to current_user.username, edit_user_path(current_user.id), class: 'nav-link'}
+              #{link_to current_user.username, edit_user_path(current_user.id), class: 'nav-link', style: 'color: black'}
             </li>
             <li class='nav-item'>
-              #{link_to 'Logout', '/logout', method: 'post', class: 'nav-link'}
+              #{link_to 'Logout', '/logout', method: 'post', class: 'nav-link btn btn-danger', style: 'color: white;'}
             </li>
           </ul>
         </div>
@@ -18,7 +18,7 @@ module ApplicationHelper
         </nav>")
     else 
         raw("<nav class='navbar navbar-expand-lg navbar-light bg-light'><div class='container-fluid'>
-          <a class='navbar-brand' href='#'>RecruitR</a>
+          <a class='navbar-brand' href='/'>RecruitR</a>
           <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'><span class='navbar-toggler-icon'></span></button>
           <div class='collapse navbar-collapse' id='navbarNav'></div>
           </div>
