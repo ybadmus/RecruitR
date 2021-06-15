cea846ddb5524fa17a89504f3410585cf8846518
ybadmus <yusif.badmus@gmail.com>
2021-06-15 17:40:55 +0000

    add ui update to interview show page

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -18,10 +18,10 @@ module InterviewsHelper
 
   def display_scorecard interview
     if interview.closed
-      raw("<h4>The interview have been closed</h4>")
+      raw("<h4>The interview have been closed</h4><h5>#{interview_score interview}</h5>")
     else 
-     
-      render 'score', score: @score
+      raw("<h6><b>Applicant scorecard (set range between 1 - 10 points)</b></h6>
+      #{render 'score', score: @score}")
     end
   end
 end
