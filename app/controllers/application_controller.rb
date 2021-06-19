e588d17b70caea2dd5f5e2141780ea6134b83501
ybadmus <yusif.badmus@gmail.com>
2021-06-19 01:39:04 +0000

    add change to code for loading skills and positons in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -71,13 +71,15 @@ class ApplicationController < ActionController::Base
 
   def client_options
     {
-      client_id: '844306511255-v6pgo5b6ksm7a2oavvjdeq0efr5acpgr.apps.googleusercontent.com',
-      client_secret: 'LLzOh-U6MVrI69x4BtPYhM8V',
+      #client_id: '844306511255-v6pgo5b6ksm7a2oavvjdeq0efr5acpgr.apps.googleusercontent.com',
+      #client_secret: 'LLzOh-U6MVrI69x4BtPYhM8V',
+      client_id: '1026907825195-eqkadq4cbdrvemcbc73ocviesadebr7p.apps.googleusercontent.com',
+      client_secret: 'fJBNnoTT1T6FLtwLKUwE_eac',
       authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
       token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
       scope: Google::Apis::CalendarV3::AUTH_CALENDAR, 
-      redirect_uri: 'https://secure-beyond-95793.herokuapp.com/callback'
-      #redirect_uri: 'http://localhost:3000/callback'
+      #redirect_uri: 'https://secure-beyond-95793.herokuapp.com/callback'
+      redirect_uri: 'http://localhost:3000/callback'
     }
   end
 end
--- app/models/position_skill.rb
+++ app/models/position_skill.rb
@@ -1,4 +1,6 @@
 class PositionSkill < ApplicationRecord
   belongs_to :skill
   belongs_to :position
+
+  validates_uniqueness_of :skill_id, scope: :position_id
 end
