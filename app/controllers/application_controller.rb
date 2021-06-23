45d172624c94708d51bbe9457a89775e186d582b
Yusif Badmus <yusif.badmus@gmail.com>
2021-06-23 18:42:42 +0000

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -72,7 +72,7 @@ class ApplicationController < ActionController::Base
   def client_options
     {
       client_id: '844306511255-v6pgo5b6ksm7a2oavvjdeq0efr5acpgr.apps.googleusercontent.com',
-      client_secret: 'LLzOh-U6MVrI69x4BtPYhM8V',
+      client_secret: '',
       authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
       token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
       scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
