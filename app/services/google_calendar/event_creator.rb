f893e5b2790711bc240a09d67a646ebd09c49242
ybadmus <yusif.badmus@gmail.com>
2021-09-25 01:54:16 +0000

    fix rubocop errors

--- app/services/google_calendar/event_creator.rb
+++ app/services/google_calendar/event_creator.rb
@@ -48,15 +48,16 @@ module GoogleCalendar
     end
 
     private
-        def client_options
-            {
-                client_id: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:client_id],
-                client_secret: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:client_secret],
-                authorization_uri: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:authorization_uri],
-                token_credential_uri: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:token_credential_uri],
-                scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
-                redirect_uri: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:redirect_uri]
-            }
-        end
+
+    def client_options
+      {
+        client_id: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:client_id],
+        client_secret: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:client_secret],
+        authorization_uri: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:authorization_uri],
+        token_credential_uri: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:token_credential_uri],
+        scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
+        redirect_uri: Rails.application.credentials[Rails.env.to_sym][:google_calendar][:redirect_uri]
+      }
+    end
   end
 end
